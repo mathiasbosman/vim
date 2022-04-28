a1899b860ebfcb0b1e3cd53134d8a79b24d0fdcc
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2022-04-28 14:26:49 +0200

    Testcontainers

--- vim-api/src/test/java/be/mathiasbosman/vim/controller/AbstractMvcTest.java
+++ vim-api/src/test/java/be/mathiasbosman/vim/controller/AbstractMvcTest.java
@@ -3,31 +3,28 @@ package be.mathiasbosman.vim.controller;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+import be.mathiasbosman.vim.AbstractSpringBootTest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
-@SpringBootTest
 @AutoConfigureMockMvc
-public abstract class AbstractControllerTest {
+public abstract class AbstractMvcTest extends AbstractSpringBootTest {
 
   @Autowired
   protected MockMvc mvc;
 
   private final ObjectMapper mapper = new ObjectMapper();
 
-  protected MockHttpServletRequestBuilder postObject(String url, Object object, boolean csrfToken)
+  protected MockHttpServletRequestBuilder postObject(String url, Object object)
       throws Exception {
 
     MockHttpServletRequestBuilder requestBuilder = post(url)
         .contentType(MediaType.APPLICATION_JSON)
         .content(mapper.writeValueAsString(object));
-    return csrfToken ? requestBuilder.with(csrf()) : requestBuilder;
+    return requestBuilder.with(csrf());
   }
 }

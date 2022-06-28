a1c85f8ce113dd1f525917e7f8e9ca2b3a846979
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2022-06-28 09:16:14 +0200

    Add simple security (for now)

--- vim-api/src/main/java/be/mathiasbosman/vim/security/SecurityConfig.java
+++ vim-api/src/main/java/be/mathiasbosman/vim/security/SecurityConfig.java
@@ -1,11 +1,17 @@
 package be.mathiasbosman.vim.security;
 
+import be.mathiasbosman.vim.security.SecurityContext.Role;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -18,9 +24,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .and()
         .authorizeRequests()
         .antMatchers("/rest/public/**").permitAll()
+        .antMatchers("/rest/**").hasRole(Role.USER)
         .and()
-        .authorizeRequests()
-        //.antMatchers("/rest/**").permitAll()
-        .anyRequest().authenticated();
+        .formLogin();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    final String password = "password";
+    auth.inMemoryAuthentication()
+        .passwordEncoder(passwordEncoder())
+        .withUser("user")
+        .password(this.passwordEncoder().encode(password))
+        .roles(Role.USER, Role.ADMIN);
+  }
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
